23e0d0e29a788aba91fa9322e2ac822b0e7d1665
tsukushibito <tsukushibito@gmail.com>
2022-05-23 17:56:23 +0900

    [WIP]

--- rust_reversi/src/reversi.rs
+++ rust_reversi/src/reversi.rs
@@ -2,7 +2,7 @@ pub mod ai_player;
 pub mod array_board;
 pub mod common;
 pub mod game;
+pub mod game_tree;
 pub mod index_board;
 pub mod indexer;
 pub mod player;
-// pub mod game_tree;
--- rust_reversi/src/reversi/game_tree.rs
+++ rust_reversi/src/reversi/game_tree.rs
@@ -1,78 +1,35 @@
-use super::board::Board;
+use crate::Board;
+use std::rc::Rc;
 
-const C_PUCT: f32 = 1.0;
-
-struct GameNode {
-    board: Board,
-    depth: u32,
-    children: Vec<GameNode>,
-    policy: f32,
-    weight: f32,
-    evaluation_count: u32,
+struct GameTreeNode<T>
+where
+    T: Board<T>,
+{
+    board: Rc<T>,
 }
 
-impl GameNode {
-    pub fn new(board: Board, depth: u32) -> GameNode {
-        GameNode {
-            board: board,
-            depth: depth,
-            children: Vec::new(),
-            policy: 0.0,
-            weight: 0.0,
-            evaluation_count: 0,
+impl<T> GameTreeNode<T>
+where
+    T: Board<T>,
+{
+    pub fn new(initial_board: T) -> GameTreeNode<T> {
+        GameTreeNode {
+            board: Rc::new(initial_board),
         }
     }
 
-    fn evaluate(&self) -> f32 {
-        if self.board.is_game_over() {
-            let (self_count, opponent_count) = if self.depth % 2 == 0 {
-                (self.board.black_count(), self.board.white_count())
-            } else {
-                (self.board.white_count(), self.board.black_count())
-            };
-
-            let value = if self_count > opponent_count {
-                1.0
-            } else if self_count < opponent_count {
-                -1.0
-            } else {
-                0.0
-            };
-
-            return value;
-        }
-        0.0f32
+    fn evaluate(&self) -> i32 {
+        0
     }
 
-    fn next_child_node(&self) -> &GameNode {
-        let total = self
-            .children
-            .iter()
-            .fold(0, |sum, c| sum + c.evaluation_count);
-
-        let values = self.children.iter().map(|c| {
-            let v = if c.evaluation_count == 0 {
-                0.0
-            } else {
-                // 相手番の評価なので-1をかける（ネガMAX）
-                -c.weight / c.evaluation_count as f32
-            };
-            v + C_PUCT * c.policy * (total as f32).sqrt() / (1.0 + c.evaluation_count as f32)
-        });
-
-        let mut index = 0usize;
-        let mut max = 0.0f32;
-        for (i, v) in values.enumerate() {
-            if v > max {
-                max = v;
-                index = i;
-            }
-        }
-
-        &self.children[index]
+    fn next_child_node(&self) -> &GameTreeNode<T> {
+        todo!()
     }
 }
 
-struct GameTree {
-    root_node: GameNode,
+struct GameTree<T>
+where
+    T: Board<T>,
+{
+    root: GameTreeNode<T>,
 }
