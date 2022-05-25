f989f70a73f563d60bb58760314937de533dd72f
tsukushibito <tsukushibito@gmail.com>
2022-05-25 18:34:01 +0900

    Refactor Game

--- rust_reversi/src/reversi/game.rs
+++ rust_reversi/src/reversi/game.rs
@@ -13,7 +13,6 @@ where
     black_player: T,
     white_player: U,
     depth: u32,
-    has_passed: bool,
     board_history: Vec<Rc<V>>,
 }
 
@@ -29,7 +28,6 @@ where
             black_player: black_player,
             white_player: white_player,
             depth: 0,
-            has_passed: false,
             board_history: Default::default(),
         }
     }
@@ -45,14 +43,8 @@ where
             };
 
             if let Some(next_board) = self.board.apply_action(&action) {
-                if let ActionType::Pass = action.action {
-                    if self.has_passed {
-                        // 2連続パスなのでゲーム終了
-                        break;
-                    }
-                    self.has_passed = true;
-                } else {
-                    self.has_passed = false;
+                if self.board.is_game_over() {
+                    break;
                 }
 
                 self.depth += 1;
@@ -66,6 +58,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::array_board::ArrayBoard;
     use crate::index_board::IndexBoard;
     use crate::indexer::Indexer;
     use crate::Action;
@@ -78,6 +71,7 @@ mod tests {
         board: IndexBoard,
     }
 
+    /// 左上優先で置けるところに置いていくプレイヤー
     impl Test1Player {
         fn new() -> Test1Player {
             let indexer = Rc::new(Indexer::new());
@@ -106,6 +100,7 @@ mod tests {
         }
     }
 
+    /// 最短でゲーム終了する手順を踏むプレイヤー
     struct Test2Player {}
 
     impl Test2Player {
@@ -138,21 +133,58 @@ mod tests {
     }
 
     #[test]
-    fn test_run() {
-        let indexer = Rc::new(Indexer::new());
-        let board = Rc::new(IndexBoard::new_initial(indexer));
-        let black = Test1Player::new();
-        let white = Test1Player::new();
-        let mut reversi = Game::new(board, black, white);
-        reversi.run();
+    fn test_index_board_run() {
+        {
+            let indexer = Rc::new(Indexer::new());
+            let board = Rc::new(IndexBoard::new_initial(indexer));
+            let black = Test1Player::new();
+            let white = Test1Player::new();
+            let mut reversi = Game::new(board, black, white);
+            reversi.run();
 
-        let indexer = Rc::new(Indexer::new());
-        let board = Rc::new(IndexBoard::new_initial(indexer));
-        let black = Test2Player::new();
-        let white = Test2Player::new();
-        let mut reversi = Game::new(board, black, white);
-        reversi.run();
+            assert_eq!(64, reversi.depth);
+            assert_eq!(19, reversi.board.black_count());
+            assert_eq!(45, reversi.board.white_count());
+        }
 
-        assert_eq!(11, reversi.depth)
+        {
+            let indexer = Rc::new(Indexer::new());
+            let board = Rc::new(IndexBoard::new_initial(indexer));
+            let black = Test2Player::new();
+            let white = Test2Player::new();
+            let mut reversi = Game::new(board, black, white);
+            reversi.run();
+
+            assert_eq!(10, reversi.depth);
+            assert_eq!(0, reversi.board.black_count());
+            assert_eq!(14, reversi.board.white_count());
+        }
+    }
+
+    #[test]
+    fn test_array_board_run() {
+        {
+            let board = Rc::new(ArrayBoard::new_initial());
+            let black = Test1Player::new();
+            let white = Test1Player::new();
+            let mut reversi = Game::new(board, black, white);
+            reversi.run();
+
+            assert_eq!(64, reversi.depth);
+            assert_eq!(19, reversi.board.black_count());
+            assert_eq!(45, reversi.board.white_count());
+        }
+
+        {
+            let board = Rc::new(ArrayBoard::new_initial());
+            let black = Test2Player::new();
+            let white = Test2Player::new();
+            let mut reversi = Game::new(board, black, white);
+            reversi.run();
+
+            assert_eq!(10, reversi.depth);
+            assert_eq!(0, reversi.board.black_count());
+            assert_eq!(14, reversi.board.white_count());
+        }
     }
 }
