fdc85f96f1baf3ead7395f01aa78dc37fdec32f8
tsukushibito <tsukushibito@gmail.com>
2022-06-16 22:35:05 +0900

    [WIP]

--- rust_reversi/src/reversi/board/bit_board.rs
+++ rust_reversi/src/reversi/board/bit_board.rs
@@ -9,18 +9,33 @@ pub struct BitBoard {
     white: u64,
 }
 
-fn continuous_line<T>(position: u64, opponent: u64, mask: u64, shift: &T) -> u64
-where
-    T: Fn(u64) -> u64,
-{
-    let mask = opponent & mask;
-    let mut result = mask & shift(position);
-    result |= mask & shift(result);
-    result |= mask & shift(result);
-    result |= mask & shift(result);
-    result |= mask & shift(result);
-    result |= mask & shift(result);
+const HORIZONTAL_MASK: u64 = 0x7e7e7e7e7e7e7e7e;
+const VERTICAL_MASK: u64 = 0x00ffffffffffff00;
+const DIAGONAL_MASK: u64 = 0x007e7e7e7e7e7e00;
+
+fn left_shift(value: u64, shift_count: u32) -> u64 {
+    value << shift_count
+}
+
+fn right_shift(value: u64, shift_count: u32) -> u64 {
+    value >> shift_count
+}
+
+fn continuous_line(position: u64, mask: u64, shift: fn(u64, u32) -> u64, shift_count: u32) -> u64 {
+    let mut result = mask & shift(position, shift_count);
+    result |= mask & shift(result, shift_count);
+    result |= mask & shift(result, shift_count);
+    result |= mask & shift(result, shift_count);
+    result |= mask & shift(result, shift_count);
+    result |= mask & shift(result, shift_count);
     result
+}
+
+fn horizontal_continuous_line(position: u64, opponent: u64) -> u64 {
+    let mask = opponent & HORIZONTAL_MASK;
+    let mut r = continuous_line(position, mask, left_shift, 1);
+    r = left_shift(r, 1);
+    r
 }
 
 impl Board for BitBoard {
