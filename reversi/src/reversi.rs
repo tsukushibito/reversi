ab0643f32699037a2ead0dbbcbda27fd5144c4c5
tsukushibito <tsukushibito@gmail.com>
2022-07-04 13:32:33 +0900

    [WIP] Creating ffi interfaces

--- reversi/src/reversi.rs
+++ reversi/src/reversi.rs
@@ -1,3 +1,5 @@
+use self::{board::BitBoard, game::Game};
+
 pub mod ai;
 pub mod board;
 pub mod common;
@@ -8,9 +10,21 @@ pub mod player;
 pub struct GameEventParamDto {}
 
 #[repr(C)]
-pub struct InitParam {
-    pub on_game_event_received: fn(&GameEventParamDto),
+pub struct ActionDto {
+    pub move_pos_row: u32,
+    pub move_pos_col: u32,
+    pub is_pass: bool,
 }
 
-#[no_mangle]
-pub extern "C" fn reversi_init() {}
+#[repr(C)]
+pub struct InitParam {
+    pub game_event_handler: fn(&GameEventParamDto),
+    pub black_player_action: fn(&GameEventParamDto) -> ActionDto,
+    pub white_player_action: fn(&GameEventParamDto) -> ActionDto,
+}
+
+// #[no_mangle]
+// pub extern "C" fn reversi_create_game() -> *mut Game {
+//     let board = Rc<BitBoard>();
+//     Game::new(board, );
+// }
