6f287a723c7625c368352f6869d9198f43b3d033
tsukushibito <tsukushibito@gmail.com>
2022-08-06 08:04:49 +0900

    Recreate plugin project

--- plugin/lib/plugin.dart
+++ plugin/lib/plugin.dart
@@ -1,9 +1,3 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. An FFI plugin project that supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin_ffi --platforms <platforms> .`
-// in this directory. You can also find a detailed instruction on how to
-// add platforms in the `pubspec.yaml` at
-// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
 import 'dart:async';
 import 'dart:ffi';
