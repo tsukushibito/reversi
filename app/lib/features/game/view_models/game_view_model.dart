992e7354bfb541ec187eb00ea7fab9197830a8ae
tsukushibito <tsukushibito@gmail.com>
2022-08-19 12:09:12 +0900

    Refactor game_view_model.dart

--- app/lib/features/game/view_models/game_view_model.dart
+++ app/lib/features/game/view_models/game_view_model.dart
@@ -7,41 +7,39 @@ class GameViewModel {
   GameViewModel(r.Board board) : _board = board;
   GameViewModel.init() : _board = r.Board.initial();
 
-  r.Board _board;
+  final r.Board _board;
 
   final _updateEventController = StreamController<GameViewModel>.broadcast();
 
   Stream<GameViewModel> get updateEvent => _updateEventController.stream;
   UnmodifiableListView<int> get squares => _board.squares;
-  String get blackCount => _board.blackCount().toString();
-  String get whiteCount => _board.whiteCount().toString();
+  String get blackCount => 'Black: ${_board.blackCount()}';
+  String get whiteCount => 'White: ${_board.whiteCount()}';
   String get message => _createMessage();
   List<r.Position> get movables => _board.getMovablePositions(_board.turn());
 
   void receivePosition(r.Position position) {
+    // 手を進める
     var action = r.Action(_board.turn(), position, false);
     var next = _board.applyAction(action);
     if (next == null) return;
-    _board = next;
-    _updateEventController.add(GameViewModel(_board));
+    _updateEventController.add(GameViewModel(next));
 
-    if (!_board.isGameOver() &&
-        _board.getMovablePositions(_board.turn()).isEmpty) {
+    // パスの場合は2秒後に自動で進める
+    if (!next.isGameOver() && next.getMovablePositions(next.turn()).isEmpty) {
       Future.delayed(const Duration(seconds: 2), () {
-        var passAct = r.Action(_board.turn(), r.Position(0, 0), true);
-        var next = _board.applyAction(passAct);
-        if (next == null) {
+        var passAct = r.Action(next.turn(), r.Position(0, 0), true);
+        var nextnext = next.applyAction(passAct);
+        if (nextnext == null) {
           throw Exception("");
         }
-        _board = next;
-        _updateEventController.add(GameViewModel(_board));
+        _updateEventController.add(GameViewModel(nextnext));
       });
     }
   }
 
   void reset() {
-    _board = r.Board.initial();
-    _updateEventController.sink.add(GameViewModel(_board));
+    _updateEventController.sink.add(GameViewModel(r.Board.initial()));
   }
 
   String _createMessage() {
